db4a58b92d58f090ddf8ff65c5608319aa1b6ad5
iVH <hoatran@hoas-macbook-pro.local>
2018-12-13 16:46:24 +0700

    Initial Commit

---
+++ Osscar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Osscar
+//
+//  Created by Hoà Trần on 12/13/18.
+//  Copyright © 2018 Hoà Trần. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
